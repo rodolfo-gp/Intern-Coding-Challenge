430b0899a635225a1b024b5c808a95d894afbda9
Inko-z <rodolfoandresgp@hotmail.com>
2025-03-16 16:47:50 -0600

    Create Bases class template to process information. Will need to fill the methods

---
+++ SensorPairer.py
@@ -0,0 +1,22 @@
+class SensorPairer:
+    def __init__(self, parameter1, parameter2):
+      
+        self.parameter1 = parameter1
+        self.parameter2 = parameter2
+
+    def method1(self):
+      
+        pass
+
+    def method2(self, param):
+    
+        pass
+
+    def __str__(self):
+    
+        return f"ClassName(parameter1={self.parameter1}, parameter2={self.parameter2})"
+
+
+if __name__ == "__main__":
+    
+    print("hello")
